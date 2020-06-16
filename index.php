9627df7f3db7df6ca59a0d2cccf998179937628a
ranjitben10 <38244278+ranjitben10@users.noreply.github.com>
2020-06-16 13:46:06 +0530

    comm1

--- index.php
+++
@@ -1,7 +0,0 @@
-Welcome To Ranjit's Site...
-<?php
-print "ipconfig"
-print "<h2>S3 Bucket Images</h2>";
-print "<img src='https://ui-ux-designs.s3.ap-south-1.amazonaws.com/EventManagerSignUp.png' width='500' height='600'>";
-?>
- 
